e38e967bce63e0fee0a731d923877db0f4d46c2a
Giaginogaucho <claudiomorenocaja@gmail.com>
2025-04-09 20:35:20 +0200

    Add files via upload

---
+++ alberi/Nodo.java
@@ -0,0 +1,18 @@
+package alberi;
+
+public class Nodo {
+    public int info;
+    public Nodo sx;
+    public Nodo dx;
+
+   public Nodo(int info) {
+    this.info = new Tipo(info);
+    this.sx = null;
+    this.dx = null;
+   }
+
+
+
+
+}
+
